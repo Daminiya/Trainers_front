2d0b3c6aad97368591d9ded67fd567163c06725e
sujaany <ssujaany09@gmail.com>
2018-12-20 15:25:07 +0530

    Profile Done

--- src/app/modules/trainer/trainer-management/profile/technology-skill-level/technology-skill-level.component.ts
+++ src/app/modules/trainer/trainer-management/profile/technology-skill-level/technology-skill-level.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatTableDataSource, MatPaginator, MatSort } from '@angular/material';
 
 @Component({
   selector: 'app-technology-skill-level',
@@ -7,9 +8,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TechnologySkillLevelComponent implements OnInit {
 
+  displayedColumns: string[] = ['technology','skill','level','edit/delete'];
+
+  technologySkill = [
+    {'technology':'Core Technology','skill':'core Java','level':'3','edit/delete':''},
+    
+  ]
+  dataSource = new MatTableDataSource<any>(this.technologySkill);
+
+
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
+
   constructor() { }
 
   ngOnInit() {
+    this.dataSource = new MatTableDataSource<any>(this.technologySkill);
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+  }
+
+  applyFilter(filterValue: string) {
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
   }
 
 }
+
