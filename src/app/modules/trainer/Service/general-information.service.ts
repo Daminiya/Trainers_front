083d40602ee676c55db9149909a14c3b300259c8
sujaany <ssujaany09@gmail.com>
2018-12-23 12:22:16 +0530

    Profile frontend done

--- src/app/modules/trainer/Service/general-information.service.ts
+++ src/app/modules/trainer/Service/general-information.service.ts
@@ -2,6 +2,10 @@ import { Injectable } from '@angular/core';
 import { HttpHeaders, HttpClient } from '@angular/common/http';
 import { GeneralInformation } from '../Model/general-information';
 
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+}
+
 @Injectable({
   providedIn: 'root'
 })
